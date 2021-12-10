d0a1e5a8120e061e9d0b9cf925ac8a7854a04560
Ruben Barros <atosc08@gmail.com>
2021-12-10 03:43:54 +0100

    sentry

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import './env';
 
 import { fastify } from 'fastify';
+import * as Sentry from '@sentry/node';
 import { PrismaClient } from '.prisma/client';
 import { routes, routesWithAuth, routesWithAuthAdmin } from './api/routes';
 
 const server = fastify();
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, SENTRY_DSN = '' } = process.env;
 
+Sentry.init({
+  dsn: SENTRY_DSN,
+});
+
+server.addHook('onError', async (_request, _reply, error) => {
+  Sentry.captureException(error);
+});
 server.register(routes);
 server.register(routesWithAuth);
 server.register(routesWithAuthAdmin);
