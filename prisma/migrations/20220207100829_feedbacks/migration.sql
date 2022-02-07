1c5d2412f6bcd54c8407728bdf958e02cdf63476
Ruben <atosc08@gmail.com>
2022-02-07 13:35:00 +0100

    Routes Feedback Api

--- prisma/migrations/20220207100829_feedbacks/migration.sql
+++ prisma/migrations/20220207100829_feedbacks/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
-CREATE TABLE "Feedback" (
+CREATE TABLE "Feedbacks" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(140) NOT NULL,
     "description" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Feedbacks_pkey" PRIMARY KEY ("id")
 );
